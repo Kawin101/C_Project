f9633276188117acce8de2897743775be8cbd0cd
Green Kawinphop <25kawinphop@gmail.com>
2023-10-30 15:39:56 +0700

    Update basic_battery_system.c

--- basic_battery_system.c
+++ basic_battery_system.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
-    print("Hello World!");
+    int battery = 100; // Starting at 100%
+    
+    for (int mins = 1; mins <= 180; mins++) {
+        printf("Battery: %d%%\n", battery);
+        
+        sleep(1); // Sleep for 1 second to 1 mins
+        battery--; // Decrease value by 1%
+        
+        if (battery == 30) {
+            printf("Battery is Low Power.\n");
+        }
+        if (battery == 15) {
+            printf("Battery is close soon!.\n");
+        }
+        if (battery == 0) {
+            printf("Batter is shutdown now!.\n");
+            break;
+        }
+    }
+    printf("Done.\n");
     return 0;
 }
